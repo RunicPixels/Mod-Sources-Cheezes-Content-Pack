a6c2942138f048abb5edd56e0585d4f393b7e759
Cheezegami <superronald@live.nl>
2016-11-28 23:45:57 +0100

    ultra heavy bouncer added.

---
+++ CheezeMod/Items/Weapons/Ratchet/UltraHeavyBouncer.cs
@@ -0,0 +1,55 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CheezeMod.Items.Weapons.Ratchet
+{
+	public class UltraHeavyBouncer : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.name = "Ultra Heavy Bouncer";
+			item.damage = 100;
+			item.ranged = true;
+			item.width = 66;
+			item.height = 42;
+            item.scale = 0.8f;
+            item.toolTip = "Uses bullets, shoots a double splitting ball that inflicts frostburn.";
+            item.toolTip2 = "Inspired by Ratchet and Clank: Going Commando.";
+            item.useTime = 46;
+            item.useAnimation = 46;
+			item.useStyle = 5;
+			item.noMelee = true; //so the item's animation doesn't do damage
+			item.knockBack = 5;
+			item.value = 1000000;
+			item.rare = 10;
+			item.useSound = 11;
+			item.autoReuse = true;
+			item.shoot = 10; //idk why but all the guns in the vanilla source have this
+			item.shootSpeed = 4f;
+			item.useAmmo = ProjectileID.Bullet;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "MegaHeavyBouncer");
+            recipe.AddIngredient(null, "LuminiteBolt", 50);
+            recipe.AddIngredient(ItemID.MartianConduitPlating, 30);
+            recipe.AddIngredient(ItemID.Nanites, 15);
+            recipe.AddTile(null, "MegaCorpVendor");
+            recipe.SetResult(this, 1);
+            recipe.AddRecipe();
+        }
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            float spread = 1f; // 1 degrees
+            speedX = CheezeMod.CalculateSpread(spread, speedX, speedY, 'X');
+            speedY = CheezeMod.CalculateSpread(spread, speedX, speedY, 'Y');
+            type = mod.ProjectileType("UltraHeavyBouncer");
+            return true;
+        }
+    }
+}
---
+++ CheezeMod/Projectiles/Ranged/UltraHeavyBouncer.cs
@@ -0,0 +1,66 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CheezeMod.Projectiles.Ranged
+{
+    public class UltraHeavyBouncer : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.CloneDefaults(ProjectileID.BoulderStaffOfEarth);
+            projectile.name = "Ultra Heavy Bouncer";
+            projectile.width = 36;
+            projectile.height = 36;
+            projectile.scale = 1.2f;
+            projectile.magic = true;
+            projectile.friendly = true;
+            projectile.timeLeft = 120;
+            projectile.penetrate = 1;
+            projectile.knockBack *= 2;
+            aiType = ProjectileID.BoulderStaffOfEarth;
+        }
+        public override void AI()
+        {
+            if (Main.rand.Next(4) == 0)
+            {
+                int num1 = Dust.NewDust(projectile.position, projectile.width, projectile.height, mod.DustType("LiquidNitrogen"), projectile.oldVelocity.X * 0.4f, projectile.oldVelocity.Y * 0.4f);
+                Main.dust[num1].noGravity = true;
+            }
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            Main.PlaySound(2, (int)projectile.position.X, (int)projectile.position.Y, 14);
+            for (int i = 0; i <= 1 +Main.rand.Next(2); i++)
+            {
+                Projectile.NewProjectile(projectile.position.X, projectile.position.Y, (projectile.oldVelocity.X * 1f) / 4 - 2 + Main.rand.Next(4), (projectile.oldVelocity.Y * 1f) / 2 - 1 - Main.rand.Next(4), mod.ProjectileType("MegaHeavyBouncer"), (int)(projectile.damage * 0.75f), projectile.knockBack / 2, projectile.owner);
+            }
+            for (int i = 0; i < Main.rand.Next(4) + 4; i++)
+            {
+                Dust.NewDust(projectile.position, projectile.width, projectile.height, mod.DustType("LiquidNitrogen"), projectile.oldVelocity.X * 0.4f, projectile.oldVelocity.Y * 0.4f);
+            }
+            for (int i = 0; i < Main.rand.Next(5) + 3; i++)
+            {
+                Dust.NewDust(projectile.position, projectile.width, projectile.height, 56, projectile.oldVelocity.X * 0.4f, projectile.oldVelocity.Y * 0.4f);
+            }
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            target.AddBuff(BuffID.Frostburn, 200);
+            base.OnHitNPC(target, damage, knockback, crit);
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            if (projectile.timeLeft > 15)
+            {
+                projectile.timeLeft = 15;
+            }
+            return base.OnTileCollide(oldVelocity);
+        }
+    }
+}
